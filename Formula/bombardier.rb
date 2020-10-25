05382107c489165ab7ac92485f21b75d8c1a855b
Maciej Karpusiewicz <maciej.karpusiewicz@outlook.com>
2020-10-25 19:05:51 +0100

    fix sha

--- Formula/bombardier.rb
+++ Formula/bombardier.rb
@@ -3,7 +3,7 @@ class Bombardier < Formula
   homepage "https://github.com/codesenberg/bombardier"
   url "https://github.com/codesenberg/bombardier/releases/download/v1.2.1/bombardier-linux-amd64"
   version "1.2.1"
-  sha256 "0a8338f93a2cfa7686c0c5836f6ab7ad65275e483fbf517df46df4d306682cc2"
+  sha256 "c09b094e8c939b5f449d83fa8d931bfc3f2e8bdcf6d3d0927018eb54d6722ad4"
   license "MIT"
 
   def install
