8a7860706449ac8c293914e396cc5502300dcc9e
Maciej Karpusiewicz <maciej.karpusiewicz@outlook.com>
2020-10-25 21:07:55 +0100

    add bump script

---
+++ bump.sh
@@ -0,0 +1,5 @@
+#URL=`curl -sI -w %{redirect_url} -o /dev/null https://github.com/codesenberg/bombardier/releases/latest/download/bombardier-linux-amd64`
+VERSION=`curl --silent "https://api.github.com/repos/codesenberg/bombardier/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | cut -c 2-`
+URL=`echo https://github.com/codesenberg/bombardier/releases/download/v${VERSION}/bombardier-linux-amd64`
+
+brew bump-formula-pr --no-browse --no-fork --url ${URL} --version ${VERSION} bombardier
