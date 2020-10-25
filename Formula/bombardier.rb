bb7d9748499c477b7b3462ecc69278f3a69a9138
Maciej Karpusiewicz <maciej.karpusiewicz@outlook.com>
2020-10-25 19:03:56 +0100

    initial commit

---
+++ Formula/bombardier.rb
@@ -0,0 +1,16 @@
+class Bombardier < Formula
+  desc "Fast cross-platform HTTP benchmarking tool written in Go"
+  homepage "https://github.com/codesenberg/bombardier"
+  url "https://github.com/codesenberg/bombardier/releases/download/v1.2.1/bombardier-linux-amd64"
+  version "1.2.1"
+  sha256 "0a8338f93a2cfa7686c0c5836f6ab7ad65275e483fbf517df46df4d306682cc2"
+  license "MIT"
+
+  def install
+    bin.install "bombardier-linux-amd64" => "bombardier"
+  end
+
+  test do
+    assert_equal "bombardier version v#{version} linux/amd64", shell_output("#{bin}/bombardier --version 2>&1").chomp
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Command-line-tools Tap
+
+## How do I install these formulae?
+`brew install command-line-tools/tap/<formula>`
+
+Or `brew tap command-line-tools/tap` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
