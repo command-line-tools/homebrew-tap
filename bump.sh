ca43bb52412193039159527ef7d4a0db8f532a97
Maciej Karpusiewicz <maciej.karpusiewicz@outlook.com>
2020-10-26 19:47:20 +0100

    don't bump if no new version

--- bump.sh
+++ bump.sh
@@ -2,4 +2,5 @@
 VERSION=`curl --silent "https://api.github.com/repos/codesenberg/bombardier/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | cut -c 2-`
 URL=`echo https://github.com/codesenberg/bombardier/releases/download/v${VERSION}/bombardier-linux-amd64`
 
-brew bump-formula-pr --no-browse --no-fork --commit --write --force --url ${URL} --version ${VERSION} bombardier
+! brew livecheck --tap=command-line-tools/tap -q --newer-only bombardier | grep -q "No newer upstream versions." && \
+    brew bump-formula-pr --no-browse --no-fork --commit --write --force --url ${URL} --version ${VERSION} bombardier
